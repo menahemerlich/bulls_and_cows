f19f7173f9e51b1417b795f44bdc3ce28729937a
menahem_erlich <erlichmenahem@gmail.com>
2025-10-29 12:17:06 +0200

    finish!!!!!!!!!!!!

--- game/validate.py
+++ game/validate.py
@@ -2,6 +2,7 @@
 def is_valid_guess(guess: str, length: int) -> tuple[bool, str]:
     if len(guess) == length and guess.isdigit():
         return True, guess
+    print("Typo error!!")
     return False, guess
 
 def is_new_guess(guess: str, history:list[str]) -> bool:
