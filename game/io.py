bd5eb03f5875a6be70618aca057d9bccd02b3e49
menahem_erlich <erlichmenahem@gmail.com>
2025-10-29 11:16:14 +0200

    i finished funcsion 'print_feedback', it's work.

--- game/io.py
+++ game/io.py
@@ -3,8 +3,10 @@ def prompt_guess() -> str:
     guess = input("Enter your guess: ")
     return guess
 
-def print_feedback(guess: str):
-    pass
+def print_feedback(guess: str, bulls: int, cows: int) -> None:
+    print(f"your guess: {guess}\n"
+          f"your bulls: {bulls}\n"
+          f"your cows: {cows}")
 
 
 
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from game.secret import generate_secret
 from game.validate import is_valid_guess, is_new_guess
 from game.logic import score_guess, is_won, game_state, apply_guess
-from game.io import prompt_guess
+from game.io import prompt_guess, print_feedback
 
 guess = prompt_guess()
 secret = generate_secret()
 game_info = game_state(secret, 4)
 # print(is_valid_guess("02002", 4))
 # print(is_new_guess("22", ["000", "224"]))
-bulls = (score_guess(secret, guess))
-print(is_won(bulls[0], 4))
+bulls_and_cows = (score_guess(secret, guess))
+print(is_won(bulls_and_cows[0], 4))
 print(game_info)
-print(apply_guess(game_info, guess, bulls))
+print(apply_guess(game_info, guess, bulls_and_cows))
 print(game_info)
+print_feedback(guess, bulls_and_cows[0], bulls_and_cows[1])
 
 
 
